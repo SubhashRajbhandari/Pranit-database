8a0f701a6e4a67d7e03bf13f852bf3b1753222be
pranitkarki <142531776+pranitkarki@users.noreply.github.com>
2023-11-14 23:56:12 +0545

    Update user_role_table.sql
    new column added

--- user_role_table.sql
+++ user_role_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE mytimesheet.user_roles (
   user_role_id INT PRIMARY KEY not null AUTO_INCREMENT,
   user_id INT NOT NULL,
   role_id INT NOT NULL,
+  role_number INT NOT NULL,
   UNIQUE KEY (user_id, role_id),
   FOREIGN KEY (user_id) REFERENCES users(user_id),
 FOREIGN KEY (role_id) REFERENCES roles(role_id)
